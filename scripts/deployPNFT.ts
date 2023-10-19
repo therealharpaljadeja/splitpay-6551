dc5d0c14c216dc860fa9c2170f6878a64ce8a898
Harpal Jadeja <jadejaharpal14@gmail.com>
2023-10-19 12:44:05 +0530

    chore: add PNFT addresses

--- scripts/deployPNFT.ts
+++ scripts/deployPNFT.ts
@@ -1,8 +1,8 @@
 import { ethers } from "hardhat";
 
-// "scroll-sepolia": "0xaC45c833E270300167b94DB759A21eAAE7eF5C78",
-//     mantle: "0xd45A1D84d62AA0976618a3B7c56D96ff0A2389c6",
-//     maticmum: "0x2c8bf7Bd8bfbF6227DedD12cDe5f2AB3d60bA1B4",
+// Scroll: "0xaC45c833E270300167b94DB759A21eAAE7eF5C78",
+// Mantle: "0xd45A1D84d62AA0976618a3B7c56D96ff0A2389c6",
+// Mumbai: "0x2c8bf7Bd8bfbF6227DedD12cDe5f2AB3d60bA1B4",
 async function mint() {
     let [deployer] = await ethers.getSigners();
     let apeNFT = await ethers.getContractAt("PNFT", "");
