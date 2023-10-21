619391e7e25bf99ffcccb8f6948c5ad4ded00efc
Harpal Jadeja <jadejaharpal14@gmail.com>
2023-10-21 14:55:08 +0530

    fix: build exclude

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -20,31 +20,31 @@ const config: HardhatUserConfig = {
             accounts: [process.env.PRIVATE_KEY!],
         },
     },
-    // etherscan: {
-    //     apiKey: {
-    //         scrollTestnet: "4ZZ95EJJX9U2KVV243DA7MU2JPZN2YR143",
-    //         polygonMumbai: "62MNZWZMP1ZZV3ZRFF4Z3FAH8II41HR2TH",
-    //         mantleTestnet: "4ZZ95EJJX9U2KVV243DA7MU2JPZN2YR143",
-    //     },
-    //     customChains: [
-    //         {
-    //             network: "scrollTestnet",
-    //             chainId: 534351,
-    //             urls: {
-    //                 apiURL: "https://sepolia-blockscout.scroll.io/api",
-    //                 browserURL: "https://sepolia-blockscout.scroll.io/",
-    //             },
-    //         },
-    //         {
-    //             network: "mantleTestnet",
-    //             chainId: 5001,
-    //             urls: {
-    //                 apiURL: "https://explorer.testnet.mantle.xyz/api",
-    //                 browserURL: "https://explorer.testnet.mantle.xyz",
-    //             },
-    //         },
-    //     ],
-    // },
+    etherscan: {
+        apiKey: {
+            scrollTestnet: "4ZZ95EJJX9U2KVV243DA7MU2JPZN2YR143",
+            polygonMumbai: "62MNZWZMP1ZZV3ZRFF4Z3FAH8II41HR2TH",
+            mantleTestnet: "4ZZ95EJJX9U2KVV243DA7MU2JPZN2YR143",
+        },
+        customChains: [
+            {
+                network: "scrollTestnet",
+                chainId: 534351,
+                urls: {
+                    apiURL: "https://sepolia-blockscout.scroll.io/api",
+                    browserURL: "https://sepolia-blockscout.scroll.io/",
+                },
+            },
+            {
+                network: "mantleTestnet",
+                chainId: 5001,
+                urls: {
+                    apiURL: "https://explorer.testnet.mantle.xyz/api",
+                    browserURL: "https://explorer.testnet.mantle.xyz",
+                },
+            },
+        ],
+    },
 };
 
 export default config;
