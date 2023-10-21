95d2adab846e1bacb027635981a2ef49142ddd86
Harpal Jadeja <jadejaharpal14@gmail.com>
2023-10-21 14:38:36 +0530

    fix: `HardhatUserConfig` error

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,6 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv";
+import { HardhatUserConfig } from "hardhat/types";
 
 dotenv.config();
 
